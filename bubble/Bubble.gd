bf8391e196d4135d169ad5d5db184c2aa25cfbc8
Jacob Grahn <jacob@grahn.io>
2020-03-26 21:31:47 -0400

    add something kind of like a map to move around in

--- bubble/Bubble.gd
+++ bubble/Bubble.gd
@@ -15,12 +15,12 @@ func _ready():
 func _input(event):
 	if event is InputEventMouseButton:
 		mouseIsDown = event.pressed
-		targetPosition = event.position
-	elif event is InputEventMouseMotion and mouseIsDown:
-		targetPosition = event.position
+		targetPosition = get_global_mouse_position()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if (mouseIsDown):
+		targetPosition = get_global_mouse_position()		
 	var vector = (targetPosition - position).normalized()
 	var dist = targetPosition.distance_to(position)
 	var throttledAcceleration = acceleration * min(dist, approachBackoff) / approachBackoff
