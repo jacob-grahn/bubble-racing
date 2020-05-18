6e1eac7398133c6e806c0874cef25342fbd50d0f
Jacob Grahn <jacob@grahn.io>
2020-05-17 22:28:08 -0400

    load music at runtime

--- maze/BeatBoxer.gd
+++ maze/BeatBoxer.gd
@@ -2,37 +2,54 @@ extends Node
 
 var elapsed = 0
 var data
-var stream
+var stream_player
+var got_stream = false
 var loudness = 0
+var json_url = "https://f000.backblazeb2.com/file/bubbleracing/songs/trumpetz.json"
+var ogg_url = "https://f000.backblazeb2.com/file/bubbleracing/songs/trumpetz.ogg"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var data_file = File.new()
-	if data_file.open("res://volume.json", File.READ) != OK:
-		return
-	var data_text = data_file.get_as_text()
-	data_file.close()
-	var data_parse = JSON.parse(data_text)
+	var JSON_request = HTTPRequest.new()
+	add_child(JSON_request)
+	JSON_request.connect("request_completed", self, "on_json_request_completed")
+	JSON_request.request(json_url)
+	
+	var OGG_request = HTTPRequest.new()
+	add_child(OGG_request)
+	OGG_request.connect("request_completed", self, "on_ogg_request_completed")
+	OGG_request.request(ogg_url)
+	
+	stream_player = get_node("../AudioStreamPlayer")
+	stream_player.connect("finished", self, "on_audio_finished")
+	
+
+func on_json_request_completed(result, response_code, headers, body):
+	var data_parse = JSON.parse(body.get_string_from_utf8())
 	if data_parse.error != OK:
 		return
 	data = data_parse.result
 	
-	stream = get_node("../AudioStreamPlayer")
-	stream.connect("finished", self, "on_audio_finished")
+	
+func on_ogg_request_completed(result, response_code, headers, body):
+	var stream = AudioStreamOGGVorbis.new()
+	stream.data = body
+	stream_player.stream = stream
+	got_stream = true
 	on_audio_finished()
-
+	
 
 func on_audio_finished():
 	elapsed = 0
-	stream.play()
+	stream_player.play()
+
 
 func _process(delta):
-	if (!data):
+	elapsed += delta	
+	if (!data || !got_stream):
 		return
-	elapsed += delta
 	var index = int(round(elapsed * 100) + 10) % data.size()
 	var dBFS = -data[index]
 	var newLoudness = max(12 + dBFS, 0) * 10
 	loudness = ((loudness * 3) + newLoudness) / 4
-	print(loudness)
